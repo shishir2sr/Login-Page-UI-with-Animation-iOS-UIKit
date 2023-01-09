3e4f80affcce0cb5f2d90da3eb4c4fcfa20e554d
Yeasir Arefin Tusher <yeasir.arefin@bjitacademy.com>
2023-01-09 14:02:46 +0600

    Logut working

--- Yeasir_30021/Controller/HomeViewController.swift
+++ Yeasir_30021/Controller/HomeViewController.swift
@@ -11,6 +11,16 @@ class HomeViewController: UIViewController {
        
         let welcomePerson = UserDefaultManager.read(key: Constants.lastLoginKey)
         self.navigationItem.prompt = "Welcome, \(welcomePerson!)"
+        self.navigationItem.hidesBackButton = true
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logoutButtonPressed))
+        self.tabBarController?.tabBar.alpha = 0
+        self.tabBarController?.tabBar.isUserInteractionEnabled = false
+        
     }
+    
 
+    
+    @objc func logoutButtonPressed(){
+        self.navigationController?.popViewController(animated: true)
+    }
 }
