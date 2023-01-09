0b307b75ffb417c51978b1ccaa4c83e7fa4c93f8
Yeasir Arefin Tusher <yeasir.arefin@bjitacademy.com>
2023-01-09 16:57:16 +0600

    Keychain functions added

--- Yeasir_30021/Controller/KeychainManager.swift
+++ Yeasir_30021/Controller/KeychainManager.swift
@@ -41,47 +41,108 @@ struct KeychainManager{
 
         return password == providedPass
     }
-
     
-    /**func readDataFromKeyChain(account: String, providedPass: String) -> Bool{
-        var passwordValid:Bool?
+    
+    //MARK: Update keychain
+    func updateKeychain(email: String, password: String) {
+        let encodedPassword = try? JSONEncoder().encode(password)
         
         let query = [
-            kSecClass : kSecClassGenericPassword,
-            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: email,
+            kSecAttrService: "password"
+        ] as CFDictionary
+        
+        let attributes = [
+            kSecValueData: encodedPassword!,
+            kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+        ] as CFDictionary
+        
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        
+        if status == errSecSuccess {
+            print("Successfully updated keychain item.")
+        } else {
+            print("Error updating keychain item:", status)
+        }
+    }
+    
+    
+    // MARK: Check if user exists
+    
+    func checkIfExistsKeychain(email: String) -> Bool {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: email,
             kSecAttrService: "password",
-            kSecReturnData: true,
-            kSecReturnAttributes: true
+            kSecReturnData: true
         ] as CFDictionary
         
         var result: CFTypeRef?
         
-        let status = SecItemCopyMatching(query, &result)
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
         
         if status == errSecSuccess {
-            if let result = result as? [CFString : Any] {
-                print(result[kSecValueData]!)
-                print(result[kSecAttrAccount]!)
-                print(result[kSecAttrService]!)
-                
-                if let data = result[kSecValueData] as? Data {
-                    let password = try? JSONDecoder().decode(String.self, from: data)
-                    print(password!)
-                    if password == providedPass{
-                        print("password match!!")
-                        passwordValid = true
-                    }
-                    else{
-                        passwordValid = false
-                        print("password not match!!")
-                    }
+            return true
+        } else {
+            return false
+        }
+    }
+
+
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**func readDataFromKeyChain(account: String, providedPass: String) -> Bool{
+    var passwordValid:Bool?
+    
+    let query = [
+        kSecClass : kSecClassGenericPassword,
+        kSecAttrAccount: account,
+        kSecAttrService: "password",
+        kSecReturnData: true,
+        kSecReturnAttributes: true
+    ] as CFDictionary
+    
+    var result: CFTypeRef?
+    
+    let status = SecItemCopyMatching(query, &result)
+    
+    if status == errSecSuccess {
+        if let result = result as? [CFString : Any] {
+            print(result[kSecValueData]!)
+            print(result[kSecAttrAccount]!)
+            print(result[kSecAttrService]!)
+            
+            if let data = result[kSecValueData] as? Data {
+                let password = try? JSONDecoder().decode(String.self, from: data)
+                print(password!)
+                if password == providedPass{
+                    print("password match!!")
+                    passwordValid = true
+                }
+                else{
+                    passwordValid = false
+                    print("password not match!!")
                 }
             }
-        } else {
-//            print(status)
-            passwordValid = false
         }
-        
-        return passwordValid!
-    }*/
-}
+    } else {
+//            print(status)
+        passwordValid = false
+    }
+    
+    return passwordValid!
+}*/
